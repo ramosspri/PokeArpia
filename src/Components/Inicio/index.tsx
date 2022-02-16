3c0b12d98d97ce057b7b5bc534629a0255f7d6c1
Carlos Eduardo <carlos.eduardo@estudante.ifgoiano.edu.br>
2022-02-16 10:07:49 -0300

    barra de pesquisa funcionando, faltam ajustes

--- src/Components/Inicio/index.tsx
+++ src/Components/Inicio/index.tsx
@@ -3,16 +3,24 @@ import axios from "axios";
 
 import { BarraStyled, BotaoPokemon, ContainerCenter, ContainerPokemon, DivBotao, ImagemPokemon, InicioStyled, ItemPokemon, NomePokemon, ParagrafoPokemon, PerfilPokemon } from './InicioStyled';
 
+interface Ipokemon {
+  name: string;
+  url: string;
+}
+
 export const Inicio: React.FC = () => {
   
   const [pokemons, setPokemons] = React.useState([]);
+  const [pokemonsPesquisado, setPokemonsPesquisado] = React.useState<Ipokemon>({name:"", url:""});
   const [proximaPagina, setProximaPagina] = React.useState("");
   const [anteriorPagina, setAnteriorPagina] = React.useState("");
   const [pesquisa, setPesquisa] = React.useState("");
   const [controllerPesquisa, setControllerPesquisa] = React.useState(false);
+  const [enterHappened, setEnterHappened] =React.useState(false);
 
   React.useEffect(() => {
     fetchPokemon("https://pokeapi.co/api/v2/pokemon?limit=18&offset=0");
+
   }, []);
 
   function pegarValor(event: React.ChangeEvent<HTMLInputElement>) {
@@ -23,11 +31,12 @@ export const Inicio: React.FC = () => {
 
   function pesquisar(event: React.KeyboardEvent<HTMLInputElement>) {
     if(event.key === "Enter") {
-      console.log(controllerPesquisa);
+      setEnterHappened(true);
       setControllerPesquisa(!controllerPesquisa);
       fetchPokemon(`https://pokeapi.co/api/v2/pokemon/${pesquisa}/`)
+    } else {
+            setEnterHappened(false)
     }
-
   }
 
   function trocarPaginaProxima() {
@@ -45,11 +54,7 @@ export const Inicio: React.FC = () => {
       if(response.data.next !== null) setProximaPagina(response.data.next);
       if(response.data.previous !== null) setAnteriorPagina(response.data.previous);
       if(controllerPesquisa) {
-        setPokemons(response.data.forms[0]);
-        console.log(response.data.forms[0].name);
-        console.log(response.data.forms[0].url);
-
-
+        setPokemonsPesquisado(response.data.forms[0]);
       }
       setPokemons(response.data.results);
     });
@@ -63,27 +68,45 @@ export const Inicio: React.FC = () => {
       </ContainerCenter>
 
       <ContainerPokemon>
+            { enterHappened &&
+                    <ItemPokemon key={pokemonsPesquisado.name}>
+                      <ImagemPokemon>
+                        <PerfilPokemon src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonsPesquisado.url.slice(39).slice(0, -1)}.png`}/>
+                      </ImagemPokemon>
+                      <NomePokemon>
+                        <div>
+                      {console.log(pokemonsPesquisado)}
+                          <ParagrafoPokemon>
+                            {
+                              pokemonsPesquisado.name.charAt(0).toUpperCase() + pokemonsPesquisado.name.slice(1)
+                            }
+                          </ParagrafoPokemon>
+                          <span style={{color:"#C9C9C9", fontSize:"15px"}}>{`#${pokemonsPesquisado.url.slice(39).slice(0, -1)}`}</span>
+                        </div>
+                      </NomePokemon>
+                    </ItemPokemon>  
+            }
 
-          {pokemons.map((pokemon:any, index:number) => {
-                {console.log(pokemon)}
-                return(
-                  <ItemPokemon key={pokemon.name}>
-                    <ImagemPokemon>
-                      <PerfilPokemon src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.url.slice(34).slice(0, -1)}.png`}/>
-                    </ImagemPokemon>
-                    <NomePokemon>
-                      <div>
-                        <ParagrafoPokemon>
-                          {
-                            pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-                          }
-                        </ParagrafoPokemon>
-                        <span style={{color:"#C9C9C9", fontSize:"15px"}}>{`#${pokemon.url.slice(34).slice(0, -1)}`}</span>
-                      </div>
-                    </NomePokemon>
-                  </ItemPokemon>  
-                );
-          })}
+            {pokemons?.map((pokemon: Ipokemon, index:number) => {
+                  return(
+                    <ItemPokemon key={pokemon.name}>
+                      <ImagemPokemon>
+                        <PerfilPokemon src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.url.slice(34).slice(0, -1)}.png`}/>
+                      </ImagemPokemon>
+                      <NomePokemon>
+                        <div>
+                          <ParagrafoPokemon>
+                            {
+                              pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+                            }
+                          </ParagrafoPokemon>
+                          <span style={{color:"#C9C9C9", fontSize:"15px"}}>{`#${pokemon.url.slice(34).slice(0, -1)}`}</span>
+                        </div>
+                      </NomePokemon>
+                    </ItemPokemon>  
+                  );
+            }  )
+          }
 
         <DivBotao>
           <BotaoPokemon onClick={trocarPaginaAnterior} id="1">Página Anterior</BotaoPokemon>
