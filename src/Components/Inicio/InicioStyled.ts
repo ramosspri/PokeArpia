766e0e6427aa7e5c083b951380da1eb569c0bbd7
Carlos Eduardo <carlos.eduardo@estudante.ifgoiano.edu.br>
2022-02-16 13:30:31 -0300

    hum

--- src/Components/Inicio/InicioStyled.ts
+++ src/Components/Inicio/InicioStyled.ts
@@ -30,7 +30,7 @@ export const ContainerPokemon = styled.div`
 export const Grid = styled.ul`
     display:grid;
     /* grid-template-columns: repeat(3,  1fr); */
-      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     gap:25px;   
     
 `
